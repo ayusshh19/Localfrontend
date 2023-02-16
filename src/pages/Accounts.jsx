daa8df879726315a599b01bfd9f77df3b0dbddae
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-02-16 22:39:42 +0530

    register speech implementation and backend done

--- src/pages/Accounts.jsx
+++ src/pages/Accounts.jsx
@@ -2,11 +2,20 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import Login from '../components/Login'
 import Register from '../components/Register'
+import {useTranslation} from 'react-i18next'
+import i18next from "i18next"
 
 export default function Accounts() {
 const [login,setlogin]=useState(false)
+const {t} = useTranslation();
+    const handleClick=(e)=>{
+        i18next.changeLanguage(e.target.value)
+        console.log(e.target.value)
+    }
   return (
     <Accountscontainer>
+        {/* <Languageselect onChange={(e)=> handleClick(e)}/>
+        <h1>{t('Enter your name')}</h1> */}
         {
             login?<Login />:<Register />
         }
@@ -15,5 +24,10 @@ const [login,setlogin]=useState(false)
 }
 
 const Accountscontainer=styled.div`
+width: 100%;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
 
 `
